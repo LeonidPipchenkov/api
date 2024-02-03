bc234acad285b34a49fcfc86a3e46d4b9671e889
Leonid Pipchenkov <t70a80@gmail.com>
2024-02-03 11:17:09 +0300

    Adding db

--- src/main/java/com/pokemonreview/api/models/Pokemon.java
+++ src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -1,10 +1,23 @@
 package com.pokemonreview.api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String type;
+
 }
--- src/main/java/com/pokemonreview/api/models/Review.java
+++ src/main/java/com/pokemonreview/api/models/Review.java
@@ -1,11 +1,24 @@
 package com.pokemonreview.api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String content;
     private int start;
+
 }
