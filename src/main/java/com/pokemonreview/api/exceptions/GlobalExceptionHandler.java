6987ad12f2960bad0719c57369c4ade5d3b97f18
Leonid Pipchenkov <t70a80@gmail.com>
2024-02-10 12:53:00 +0300

    Adding exception handler

---
+++ src/main/java/com/pokemonreview/api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.pokemonreview.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(PokemonNotFoundException.class)
+    public ResponseEntity<ErrorObject> handlePokemonNotFoundException(PokemonNotFoundException ex) {
+        ErrorObject errorObject = new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
+        return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
+    }
+
+}
